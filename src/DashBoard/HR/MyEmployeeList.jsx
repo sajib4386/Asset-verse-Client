dd28903742a4ee72aaf81da604e16d27b162b1df
Shahriar Hosssain <shahriar4386@gmail.com>
2025-12-14 21:35:57 +0600

    Implement Employeelist Delete Operation

--- src/DashBoard/HR/MyEmployeeList.jsx
+++ src/DashBoard/HR/MyEmployeeList.jsx
@@ -3,12 +3,13 @@ import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import useAuth from "../../Hooks/useAuth";
 import { useQuery } from "@tanstack/react-query";
 import Loading from "../../Components/Loading/Loading";
+import Swal from "sweetalert2";
 
 const MyEmployeeList = () => {
     const axiosSecure = useAxiosSecure();
     const { user, loading } = useAuth();
 
-    const { data = {} } = useQuery({
+    const { data = {}, refetch } = useQuery({
         queryKey: ["employees", user?.email],
         enabled: !!user?.email,
         queryFn: async () => {
@@ -18,6 +19,43 @@ const MyEmployeeList = () => {
     });
 
     const { result: employees = [], used = 0, limit = 0 } = data;
+
+    const handleEmployeeDelete = (emp) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.patch(`/hr/remove-employee`, {
+                    hrEmail: user.email,
+                    employeeEmail: emp.email
+                })
+                    .then(res => {
+                        if (res.data.deletedCount)
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your Employee has been deleted.",
+                                icon: "success"
+                            });
+                        refetch()
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        Swal.fire({
+                            title: "Error!",
+                            text: "Something went wrong while deleting.",
+                            icon: "error"
+                        });
+                    });
+
+            }
+        });
+    }
 
     if (loading) {
         return <Loading></Loading>
@@ -50,6 +88,7 @@ const MyEmployeeList = () => {
                         </p>
 
                         <button
+                            onClick={() => handleEmployeeDelete(emp)}
                             className="btn btn-sm btn-secondary mt-2 w-full"
                         >
                             Remove from Team
