74e7b14b7a5de2e7ffb6935bcece93d04e87e624
Shahriar Hosssain <shahriar4386@gmail.com>
2026-01-19 20:18:29 +0600

    Update frontend URL after deployment

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://asset-verse-server-ashen.vercel.app'
 })
 
 const useAxios = () => {
